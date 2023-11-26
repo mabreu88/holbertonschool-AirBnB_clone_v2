f47899128194ebe86c2ffdea086e8a27e82eb6cc
mabreu88 <martymalkav7@gmail.com>
2023-11-26 12:07:07 -0800

    relationship review

--- models/user.py
+++ models/user.py
@@ -17,9 +17,11 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    
+
     if typeStorage == 'db':
         places = relationship('Place', backref="user", cascade="all, delete")
+        reviews = relationship("Review", backref="user",
+                               cascade="all, delete-orphan")
     else:
         email = ""
         password = ""
